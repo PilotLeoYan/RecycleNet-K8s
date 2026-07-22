ede3386a36d12a78314f75d2d04746e132c728b1
LeoYan <leofabyano@gmail.com>
2026-07-21 20:29:48 -0600

    refactor: fix mypy false errors

--- tests/test_logger.py
+++ tests/test_logger.py
@@ -3,20 +3,20 @@ import logging
 from src.utils.logger import get_logger
 
 
-def test_get_logger_returns_logger_instance():
+def test_get_logger_returns_logger_instance() -> None:
     """"""
     logger = get_logger("test_logger")
     assert isinstance(logger, logging.Logger)
 
 
-def test_get_logger_sets_correct_name():
+def test_get_logger_sets_correct_name() -> None:
     """"""
     logger_name = "logger_of_test"
     logger = get_logger(logger_name)
     assert logger.name == logger_name
 
 
-def test_get_logger_singleton_behavior():
+def test_get_logger_singleton_behavior() -> None:
     """"""
     logger1 = get_logger("singleton_test")
     logger2 = get_logger("singleton_test")
