9f670695dd1a9976077db861f0c538c89d027a13
LeoYan <leofabyano@gmail.com>
2026-08-18 22:02:37 -0600

    feat: add __init__ and __main__

--- src/components/__init__.py
+++ src/components/__init__.py
@@ -1,11 +1,21 @@
 from .data_ingestion import DataIngestion, DataIngestionConfig
 from .data_transform import DataTransformation, DataTransformationConfig
+from .evaluator import Evaluator
+from .log_model import LogModel
+from .loss_functions import get_criterion
+from .model import build_mobilenet_v3
+from .optimizers import get_optimizer
+from .trainer import ModelTrainer
 
 __all__ = [
     "DataIngestionConfig",
     "DataIngestion",
     "DataTransformation",
     "DataTransformationConfig",
+    "build_mobilenet_v3",
+    "ModelTrainer",
+    "get_criterion",
+    "get_optimizer",
+    "Evaluator",
+    "LogModel",
 ]
-
-__version__ = "1.1.0"
