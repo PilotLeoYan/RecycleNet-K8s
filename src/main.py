74143c07a8990635c2dd75260f4ef3bb95cfd4dd
LeoYan <leofabyano@gmail.com>
2026-08-18 21:53:25 -0600

    chore: use try-catchs with new logger

--- src/main.py
+++ src/main.py
@@ -1,11 +1,26 @@
-from utils import get_logger
+import sys
+
+from src.pipeline.train_pipeline import TrainingPipelineConfig, TrainPipeline
+from src.utils import get_logger
 
 logger = get_logger(__name__)
 
 
 def main() -> None:
-    logger.info("Starting project")
+    logger.info("Starting RecycleNet")
+
+    try:
+        config = TrainingPipelineConfig()
+        pipeline = TrainPipeline(config)
+        pipeline.run()
+
+        logger.info("Pipeline successfully completed")
+    except Exception as e:
+        logger.exception("Critical error whilst running the project: %s", str(e))
+        sys.exit(1)
 
 
 if __name__ == "__main__":
     main()
+
+# python -m src.main
